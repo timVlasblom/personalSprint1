0d27836cc499b14e08b8aadfa410d12a647962ae
Tim Vlasblom <Tim Vlasblom>
2017-03-21 10:13:25 +0100

    made gruntfile cleaner and added uglify

--- breakout-Grunt/Gruntfile.js
+++ breakout-Grunt/Gruntfile.js
@@ -1,21 +1,31 @@
+'use strict';
+
 module.exports = function(grunt) {
 
 grunt.initConfig({
 	concat: {
 		dist:{
-			src:['scripts/script.js', 'scripts/peddle.js', 'scripts/bricks.js', 'scripts/ball.js'],
+			src:['scripts/**.js'],
 			dest: 'build/script.js',
 		},
 	},
 	watch:{
 		js: {
 			files: ['scripts/**/*.js'],
-			tasks: ['concat'],
+			tasks: ['concat','uglify'],
 		},
+	},
+	uglify: {
+  	dist: {
+	    files: {
+	      'build/script.min.js': ['<%= concat.dist.dest %>'],
+	    },
+	  },
 	},
 });
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask('default', ['concat', 'watch']);
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.registerTask('default', ['concat', 'watch', 'uglify']);
 
 };
